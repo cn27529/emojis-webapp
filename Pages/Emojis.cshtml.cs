18f9a496b482777abe87610fa4f1d606542131b2
cn27529 <cn27529@hotmail.com>
2023-09-30 18:27:26 +0800

    create Emojis page

---
+++ Pages/Emojis.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace emojis_webapp.Pages;
+
+public class EmojisModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
